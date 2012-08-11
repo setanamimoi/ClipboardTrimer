ceaf8f484407f4bf5e1ad9f117bfc5e7b62db16d
setanamimoi <setanamimoi@gmail.com>
2012-08-12 01:25:41 +0900

    メインプロジェクトを追加

---
+++ ClipboardTrimer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ClipboardTrimer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
