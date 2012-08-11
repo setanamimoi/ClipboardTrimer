0105ee8278f01aa72fb75834fd26ae8f93160a6c
setanamimoi <setanamimoi@gmail.com>
2012-08-12 01:23:08 +0900

    テンプレートソリューションを適用

---
+++ _Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace _Tests
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Process.Start(@"Tests.nunit");
+        }
+    }
+}
