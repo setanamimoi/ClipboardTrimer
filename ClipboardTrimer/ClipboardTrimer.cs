92db027fc9fc05d07a22da1226e092c3146e575e
setanamimoi <setanamimoi@gmail.com>
2012-08-14 02:38:36 +0900

    正常系の処理を追加

---
+++ ClipboardTrimer/ClipboardTrimer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ClipboardTrimer
+{
+    public class ClipboardTrimer
+    {
+        public static string Execute(string inputClipboardText, string prefix)
+        {
+            List<string> clipboardLineList = new List<string>();
+
+            using (StringReader reader = new StringReader(inputClipboardText))
+            {
+                for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                {
+                    clipboardLineList.Add(line);
+                }
+            }
+
+            List<string> trimeEndLineList = new List<string>();
+            foreach (string line in clipboardLineList)
+            {
+                trimeEndLineList.Add(line.TrimEnd());
+            }
+
+            List<string> unEmptyList = new List<string>();
+            foreach (string line in trimeEndLineList)
+            {
+                if (string.IsNullOrEmpty(line) == false)
+                {
+                    unEmptyList.Add(line);
+                }
+            }
+
+            List<string> withPrefixList = new List<string>();
+            foreach (string line in unEmptyList)
+            {
+                withPrefixList.Add(string.Format("{0}{1}", prefix, line));
+            }
+
+            List<string> trimedList = new List<string>();
+            foreach (string line in withPrefixList)
+            {
+                trimedList.Add(line.Trim());
+            }
+
+            List<string> ret = new List<string>();
+            foreach (string line in trimedList)
+            {
+                if (string.IsNullOrEmpty(line) == false)
+                {
+                    ret.Add(line);
+                }
+            }
+
+            string clipboardSetText = null;
+            using (StringWriter writer = new StringWriter())
+            {
+                foreach (string line in ret.Take(ret.Count - 1))
+                {
+                    writer.WriteLine(line);
+                }
+                //最終行は改行なしとする
+                writer.Write(ret[ret.Count - 1]);
+
+                clipboardSetText = writer.ToString();
+
+            }
+            return clipboardSetText;
+        }
+    }
+}
--- ClipboardTrimer/Program.cs
+++ ClipboardTrimer/Program.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ClipboardTrimer
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
@@ -15,6 +16,59 @@ namespace ClipboardTrimer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            string inputClipboardText = Clipboard.GetText();
+            string clipboardSetText = Execute(inputClipboardText);
+
+            Clipboard.SetText(clipboardSetText);
+        }
+
+        public static string Execute(this string inputClipboardText)
+        {
+            List<string> lineList = new List<string>();
+            using (var reader = new StringReader(inputClipboardText))
+            {
+                for (string line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                {
+                    lineList.Add(line);
+                }
+            }
+
+            List<string> trimEndList = new List<string>();
+            foreach (string line in lineList)
+            {
+                trimEndList.Add(line.TrimEnd());
+            }
+
+            List<string> unEmptyList = new List<string>();
+            foreach (string line in trimEndList)
+            {
+                if (string.IsNullOrEmpty(line) == false)
+                {
+                    unEmptyList.Add(line);
+                }
+            }
+
+            List<string> ret = new List<string>();
+            foreach (string line in unEmptyList)
+            {
+                ret.Add(string.Format("'{0}", line));
+            }
+
+            string clipboardSetText = null;
+            using (StringWriter writer = new StringWriter())
+            {
+                foreach (string line in ret.Take(ret.Count - 1))
+                {
+                    writer.WriteLine(line);
+                }
+                //最終行は改行なしとする
+                writer.Write(ret[ret.Count - 1]);
+
+                clipboardSetText = writer.ToString();
+
+            }
+            return clipboardSetText;
         }
     }
 }
